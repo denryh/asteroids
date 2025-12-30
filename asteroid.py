332c19c9c8651bbd966475daa323ae3eef702ff2
denryh <dg.thdat.dev@gmail.com>
2025-12-30 17:38:41 +0700

    feat(asteroid): add splitting

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
-from circleshape import CircleShape
+import random
 import pygame
-from constants import LINE_WIDTH
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split") 
+        angle = random.uniform(20, 50)
+        first_vector = self.velocity.rotate(angle)
+        second_vector = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_asteroid = Asteroid(*self.position, new_radius)
+        second_asteroid = Asteroid(*self.position, new_radius)
+
+        first_asteroid.velocity = first_vector * 1.2
+        second_asteroid.velocity = second_vector * 1.2
